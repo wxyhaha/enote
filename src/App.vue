2d48ff2a1e32236a18bb96e519dd720a86729d06
joel <1666464510@qq.com>
2021-10-14 11:46:42 +0800

    添加侧边栏

---
+++ src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <div id="app">
+    <sidebar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import sidebar from "./components/Sidebar";
+export default {
+  name: 'App',
+  components: {sidebar}
+}
+</script>
+
+<style>
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+* {
+  box-sizing: border-box;
+ margin: 0;
+ padding: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+body{
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  font-size: 14px;
+  -moz-osx-font-smoothing: grayscale;
+  background-color: #eee;
+}
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul, li{
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0px 0px 2px 0px #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+
+#app {
+  display: flex;
+  align-items: stretch;
+}
+</style>
