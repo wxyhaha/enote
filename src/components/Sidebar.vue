1f59bab4a7e39eb69138737129e3d8a0cbbf213f
joel <1666464510@qq.com>
2021-10-15 11:21:36 +0800

    引入less

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -20,37 +20,33 @@ import avatar from '@/components/Avatar'
 
   </script>
 
-<style scoped>
-
+<style lang="less" scoped>
 #sidebar {
   position: relative;
   width: 56px;
   text-align: center;
   background-color: #2c333c;
-}
+  .icons {
+    margin-top: 15px;
 
-.icons {
-  margin-top: 15px;
-}
+    a {
+      padding: 6px 0;
+      display: block;
+    }
 
-.icons a {
-  padding: 6px 0;
-  display: block;
-}
-
-.icons .router-link-active {
-  background-color: #5e6266;
-}
-
-.logout {
-  position: absolute;
-  bottom: 20px;
-  width: 100%;
-  text-align: center;
-  cursor: pointer;
-}
-
-.iconfont {
-  color: #fff;
+    .router-link-active {
+      background-color: #5e6266;
+    }
+  }
+  .logout {
+      position: absolute;
+      bottom: 20px;
+      width: 100%;
+      text-align: center;
+      cursor: pointer;
+    }
+    .iconfont {
+      color: #fff;
+    }
 }
 </style>
