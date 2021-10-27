51b9b7953eb1edc9208963305a9733685b6ec3fb
joel <1666464510@qq.com>
2021-10-27 22:01:21 +0800

    使用路由懒加载优化体验

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Login from "../components/Login";
-import NotebookList from '@/components/NotebookList'
-import NoteDetail from '@/components/NoteDetail'
-import TrashDetail from '@/components/TrashDetail'
-
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/login',
-      component: Login
-},
+      path: '/',
+      alias: '/notebooks',
+      component: ()=>import('../components/NotebookList')
+    },
     {
-        path: '/notebooks',
-       component: NotebookList
-  },
-   {
-     path: '/note',
-       component: NoteDetail
-   },
-  {
-     path: '/trash',
-       component: TrashDetail
-   }
-]
+      path: '/login',
+      component: ()=>import('../components/Login')
+    },
+    {
+      path: '/note',
+      component: ()=>import('../components/NoteDetail')
+    },
+    {
+      path: '/trash',
+      component: ()=>import('../components/TrashDetail')
+    }
+  ]
 })
